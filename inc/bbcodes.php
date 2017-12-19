5b4f2b5a33c155edceb8b6f58cd994d8b0918463
mheiduk <marc@heiduk.me>
2017-12-19 11:15:40 +0100

    Add bbcode for all shares, rename functions

--- inc/bbcodes.php
+++ inc/bbcodes.php
@@ -4,34 +4,47 @@
  */
 
 // BBCode Facebook
-function bbcode_facebook() {
+function sharecount_bbcode_facebook() {
     global $post;
 	$shares = get_post_meta($post->ID, 'sharecount_facebook', true);
 
     return empty($shares) ? 0 : $shares;
 }
-add_shortcode('sharecount_facebook', 'bbcode_facebook');
+add_shortcode('sharecount_facebook', 'sharecount_bbcode_facebook');
 
 // BBCode Twitter
-function bbcode_twitter() {
+function sharecount_bbcode_twitter() {
     global $post;
 	$shares = get_post_meta($post->ID, 'sharecount_twitter', true);
 
     return empty($shares) ? 0 : $shares;
 }
-add_shortcode('sharecount_twitter', 'bbcode_twitter');
+add_shortcode('sharecount_twitter', 'sharecount_bbcode_twitter');
 
 // BBCode Mail
-function bbcode_mail() {
+function sharecount_bbcode_mail() {
     global $post;
 	$shares = get_post_meta($post->ID, 'sharecount_mail', true);
 
     return empty($shares) ? 0 : $shares;
 }
-add_shortcode('sharecount_mail', 'bbcode_mail');
+add_shortcode('sharecount_mail', 'sharecount_bbcode_mail');
+
+// BBCode Mail
+function sharecount_bbcode_allshares() {
+    global $post;
+	$shares_facebook = get_post_meta($post->ID, 'sharecount_facebook', true);
+	$shares_twitter = get_post_meta($post->ID, 'sharecount_twitter', true);
+	$shares_mail = get_post_meta($post->ID, 'sharecount_mail', true);
+
+	$shares = $shares_facebook + $shares_twitter + $shares_mail;
+
+    return empty($shares) ? 0 : $shares;
+}
+add_shortcode('sharecount_allshares', 'sharecount_bbcode_allshares');
 
 // BBCode display buttons
-function bbcode_buttons() {
+function sharecount_bbcode_buttons() {
 	return sharecount_display();
 }
-add_shortcode('sharecount_buttons', 'bbcode_buttons');
+add_shortcode('sharecount_buttons', 'sharecount_bbcode_buttons');
